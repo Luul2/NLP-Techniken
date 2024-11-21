8fba36931842f9035dfe350caafd6f8367d84a47
Luul2 <luisaholub@web.de>
2024-11-21 11:43:44 +0100

    Funktion zur Bereinigung der Bewertungen

--- Textvorverarbeitung.py
+++ Textvorverarbeitung.py
@@ -4,11 +4,14 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-datafile = r"C:\Users\Maik und Luisa\Downloads\archive\DisneylandReviews.csv"
-data = pd.read_csv(datafile, encoding='ISO-8859-1')
-print(data.info())
-
-data.to_csv('reviews.csv', index=False)
 data = pd.read_csv('reviews.csv')
 reviews = data['Review_Text']
+
+def preprocess_text(text):
+    tokens = word_tokenize(text.lower())
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in tokens if word.isalpha() and word not in stop_words]
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
+    return ' '.join(words)
 
