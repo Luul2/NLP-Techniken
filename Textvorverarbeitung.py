44e3877bc2d2636c175ff07687226e221bc52358
Luul2 <luisaholub@web.de>
2024-11-21 11:54:52 +0100

    Überprüfen und speichern der bereinigten Bewertungen

--- Textvorverarbeitung.py
+++ Textvorverarbeitung.py
@@ -7,6 +7,9 @@ from nltk.stem import WordNetLemmatizer
 data = pd.read_csv('reviews.csv')
 reviews = data['Review_Text']
 
+nltk.download('all')
+
+
 def preprocess_text(text):
     tokens = word_tokenize(text.lower())
     stop_words = set(stopwords.words('english'))
@@ -14,4 +17,9 @@ def preprocess_text(text):
     lemmatizer = WordNetLemmatizer()
     words = [lemmatizer.lemmatize(word) for word in words]
     return ' '.join(words)
+
+
+data['Clean_Reviews'] = data['Review_Text'].apply(preprocess_text)
+print(data[['Review_Text', 'Clean_Reviews']].head())
+data.to_csv('clean_reviews.csv', index=False)
 
