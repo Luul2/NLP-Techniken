b3edf6a3300429cc24b84ddf6aca0e42f166b413
Luul2 <luisaholub@web.de>
2024-11-21 11:38:02 +0100

    Speichern der Bewertungen als reviews.csv

--- Textvorverarbeitung.py
+++ Textvorverarbeitung.py
@@ -7,5 +7,8 @@ from nltk.stem import WordNetLemmatizer
 datafile = r"C:\Users\Maik und Luisa\Downloads\archive\DisneylandReviews.csv"
 data = pd.read_csv(datafile, encoding='ISO-8859-1')
 print(data.info())
+
+data.to_csv('reviews.csv', index=False)
+data = pd.read_csv('reviews.csv')
 reviews = data['Review_Text']
 
